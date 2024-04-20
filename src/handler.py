73cb05eb81954a19c1ff393d674df8b7bde54cba
Roman Koropets <Roman.coropets@gmail.com>
2024-04-20 16:37:18 +0800

    3.0.0

--- src/handler.py
+++ src/handler.py
@@ -11,7 +11,8 @@ import time
 # You will want models to be loaded into memory before starting serverless.
 
 try:
-    pipe = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+    # pipe = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+    pipe = StableDiffusionXLPipeline.from_single_file("https://civitai-delivery-worker-prod.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/model/2384906/socababesTurbo12.2zTA.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22socababesTurboXL_v12Hybrid.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=e01358d793ad6966166af8b3064953ad/20240420/us-east-1/s3/aws4_request&X-Amz-Date=20240420T083625Z&X-Amz-SignedHeaders=host&X-Amz-Signature=d2e781e5c9968edf658f9f70860aa98304e4f1448c4cb2c207e2984c8ef6e447", torch_dtype=torch.float16, use_safetensors=True, variant="fp16", add_watermarker=False)
     pipe.to("cuda")
 except RuntimeError:
     quit()
@@ -22,7 +23,7 @@ def handler(job):
     prompt = job_input['prompt']
 
     time_start = time.time()
-    image = pipe(prompt=prompt, height=1024, width=1024, num_inference_steps=25, guidance_scale=7.5).images[0]
+    image = pipe(prompt=prompt, height=1024, width=1024, num_inference_steps=10, guidance_scale=2.0).images[0]
     print(f"Time taken: {time.time() - time_start}")
 
     buffer = io.BytesIO()
